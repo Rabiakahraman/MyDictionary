0007f28a2f9f99dc33153e26b57c915b08040881
Rabia <acer@ACER-BILGISAYAR>
2021-01-25 22:16:58 +0300

    Add project files.

---
+++ MyList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyList<T,K>
+    {
+      
+        T[] _key;
+        K[] _value;
+        T[] _tempKey;
+        K[] _tempValue;
+
+        public MyList()
+        {
+            _key = new T[0];
+            _value = new K[0];
+        }
+
+
+        public void Add(T key, K value)
+        {
+            _tempKey = _key;
+            _key = new T[_key.Length + 1];
+            for (int i = 0; i <_tempKey.Length; i++)
+            {
+                _key[i] = _tempKey[i];
+
+            }
+
+            _key[_key.Length - 1] = key;
+
+            _tempValue = _value;
+            _value = new K[_value.Length + 1];
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                _value[i] = _tempValue[i];
+            }
+            _value[_value.Length - 1] = value;
+        }    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<int,string> kisi = new MyList<int,string>();
+            kisi.Add(21,"rabia");
+            kisi.Add(32, "zeynep");
+
+
+        }
+    }
+}
